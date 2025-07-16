8e194d7407b84f4fb2ae9b3d85ad4b4f89606f52
Mikail Hamhoev <mikailhamhoev08@gmail.com>
2025-07-17 05:36:23 +0700

    Lesson3

---
+++ Lesson3/Integers.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val maxIntegerValue = Int.MAX_VALUE
+    val minIntegerValue = Int.MIN_VALUE
+
+    println("Maximum integer value is $maxIntegerValue")
+    println("Minimum integer value is $minIntegerValue")
+}
