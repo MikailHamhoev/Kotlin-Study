54e88def75972a8df895786502e3c03386a5d00b
Mikail Hamhoev <mikailhamhoev08@gmail.com>
2025-07-18 00:17:34 +0700

    Lesson4

---
+++ Lesson4/Types.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val maxIntegerValue = Int.MAX_VALUE
+    val minIntegerValue = Int.MIN_VALUE
+    println("Maximum integer value is $maxIntegerValue")
+    println("Minimum integer value is $minIntegerValue")
+    println()
+
+    val maxShortValue = Short.MAX_VALUE
+    val minShortValue = Short.MIN_VALUE
+    println("Maximum short value is $maxShortValue")
+    println("Minimum short value is $minShortValue")
+    println()
+
+    val maxByteValue = Byte.MAX_VALUE
+    val minByteValue = Byte.MIN_VALUE
+    println("Maximum byte value is $maxByteValue")
+    println("Minimum byte value is $minByteValue")
+    println()
+
+    val maxLongValue = Long.MAX_VALUE
+    val minLongValue = Long.MIN_VALUE
+    println("Maximum long value is $maxLongValue")
+    println("Minimum long value is $minLongValue")
+
+    val newInt = 3   // Int
+    val newLong = 3L // Long
+}
