43437d4cd7b8999e028bf54a0fa70e8c4531026f
Mikail Hamhoev <mikailhamhoev08@gmail.com>
2025-07-19 00:21:18 +0700

    ADD Lesson5

---
+++ Lesson5/FloatsAndDoubles.kt
@@ -0,0 +1,25 @@
+fun main() {
+    // Float
+    val oneFloat : Float = 1.0f
+    val twoFloat = 2f
+    val threeFloat = 3F
+    val fourFloat = 4.0F
+
+    // Double
+    val fiveDouble : Double = 5.0
+    val sixDouble = 6.0
+
+    println("This is a float four: $fourFloat, and here is a double six: $sixDouble.")
+    println()
+
+    val maxFloatValue = Float.MAX_VALUE
+    val minFloatValue = Float.MIN_VALUE
+    println("Maximum float value is $maxFloatValue")
+    println("Minimum float value is $minFloatValue")
+    println()
+
+    val maxDoubleValue = Double.MAX_VALUE
+    val minDoubleValue = Double.MIN_VALUE
+    println("Maximum double value is $maxDoubleValue")
+    println("Minimum double value is $minDoubleValue")
+}
