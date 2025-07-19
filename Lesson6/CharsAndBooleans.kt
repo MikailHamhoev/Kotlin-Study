e92ee23b954a9cbcf4a1493a5b3f86db5c8d7e36
Mikail Hamhoev <mikailhamhoev08@gmail.com>
2025-07-19 15:41:29 +0700

    ADD Lesson6

---
+++ Lesson6/CharsAndBooleans.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val firstExamMark: Char = 'A'
+    val isFirstExamPassed: Boolean = true
+    println("The first exam mark: $firstExamMark")
+    print("The student passed, right? - ")
+    println(isFirstExamPassed)
+    println()
+
+    val secondExamMark = 'D'
+    val isSecondExamPassed = false
+    print(secondExamMark)
+    println(" is the mark the second student received.\nIt is successful too, right? - $isSecondExamPassed")
+    println()
+}
